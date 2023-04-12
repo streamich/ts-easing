a9d66e9962cf375d8613d25b0f37726929d72035
Samet Tutkun <tutkun@users.noreply.github.com>
2023-04-12 05:19:11 +0300

    refactor: refactors `IEasingMap` interface

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,8 @@
 export type TEasing = (time: number) => number;
 
 export interface IEasingMap {
-  linear: TEasing;
-  quadratic: TEasing;
-  cubic: TEasing;
-  elastic: TEasing;
-  inQuad: TEasing;
-  outQuad: TEasing;
-  inOutQuad: TEasing;
-  inCubic: TEasing;
-  outCubic: TEasing;
-  inOutCubic: TEasing;
-  inQuart: TEasing;
-  outQuart: TEasing;
-  inOutQuart: TEasing;
-  inQuint: TEasing;
-  outQuint: TEasing;
-  inOutQuint: TEasing;
-  inSine: TEasing;
-  outSine: TEasing;
-  inOutSine: TEasing;
-  inExpo: TEasing;
-  outExpo: TEasing;
-  inOutExpo: TEasing;
-  inCirc: TEasing;
-  outCirc: TEasing;
-  inOutCirc: TEasing;
+  // "anyString": TEasing;
+  [key: string]: TEasing;
 }
 
 export const easing: IEasingMap = {
